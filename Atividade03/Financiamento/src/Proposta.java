a3662bd3a6a10b1ef9cf2883037cab603081540d
HarrisonBorges <harrisonborges@outlook.com.br>
2018-08-15 23:23:49 -0300

    Add files via upload

---
+++ Atividade03/Financiamento/src/Proposta.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Aluno
+ */
+public class Proposta {
+
+    private float total;
+    private int numeroParcelas;
+    private float valorParcelas;
+
+    Proposta() {
+
+    }
+
+    Proposta(float pTotal, int pNumeroParcelas) {
+        this.total = pTotal;
+        this.numeroParcelas = pNumeroParcelas;
+    }
+
+    /**
+     * @return the total
+     */
+    public float getTotal() {
+        return total;
+    }
+
+    /**
+     * @param total the total to set
+     */
+    public void setTotal(float total) {
+        this.total = total;
+    }
+
+    /**
+     * @return the numeroParcelas
+     */
+    public int getNumeroParcelas() {
+        return numeroParcelas;
+    }
+
+    /**
+     * @param numeroParcelas the numeroParcelas to set
+     */
+    public void setNumeroParcelas(int numeroParcelas) {
+        this.numeroParcelas = numeroParcelas;
+    }
+
+    /**
+     * @return the valorParcelas
+     */
+    public float getValorParcelas() {
+        return total / numeroParcelas;
+    }
+
+   
+
+    
+    
+    
+
+}
